c34d61758c57bad79a2ba31f34e9bc010d5ab32e
osborn israel <israelosborn94@yahoo.com>
2025-04-15 17:03:32 +0100

    first phase of onboarding

--- reg/registration_complete.py
+++
@@ -1,9 +0,0 @@
-
-
-from django.shortcuts import render
-from django.views import View
-
-
-class Complete_Reg(View):
-     def get(self, request):
-        return render(request, 'reg/registration_complete.html')
--- static/assets/js/pages/forms/editors.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-    //CKEditor
-    CKEDITOR.replace('ckeditor.html');
-    CKEDITOR.config.height = 300;
-  
-});
